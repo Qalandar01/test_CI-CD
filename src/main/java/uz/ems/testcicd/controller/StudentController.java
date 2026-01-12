101a0be8f7875e3f01ddc62ffae35f721762e84d
Qalandar01 <qalandar2201@gmail.com>
2026-01-12 23:57:34 +0500

    new line added

--- src/main/java/uz/ems/testcicd/controller/StudentController.java
+++ src/main/java/uz/ems/testcicd/controller/StudentController.java
@@ -18,6 +18,7 @@ public class StudentController {
                 new Student(2L, "Vali", 21),
                 new Student(3L, "Sardor", 22),
                 new Student(4L, "Jasmin", 20),
+
                 new Student(5L, "John", 23),
                 new Student(6L, "Anna", 19),
                 new Student(7L, "David", 22),
